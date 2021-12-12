43989f7016a6b3e917b63f0051d9d503adc532f5
zq6A_qVXaE <84458031+HiimEiffel@users.noreply.github.com>
2021-12-12 18:34:30 +0800

    Add files via upload

---
+++ RetroClash/Resources.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using RetroClash.Database;
+using RetroClash.Database.Caching;
+using RetroClash.Files;
+using RetroClash.Logic.Manager;
+using RetroClash.Network;
+using RetroClash.Protocol;
+
+namespace RetroClash
+{
+    public class Resources : IDisposable
+    {
+        private static MessageFactory _messagefactory;
+        private static CommandFactory _commandfactory;
+        private static DebugCommandFactory _debugcommandfactory;
+        private static MySQL _mysql;
+        private static ApiService _apiService;
+        private static Logger _logger;
+
+        public Resources()
+        {
+            Configuration = new Configuration();
+            Configuration.Initialize();
+
+            _logger = new Logger();
+
+            Csv = new Csv();
+            Fingerprint = new Fingerprint();
+
+            _mysql = new MySQL();
+            _messagefactory = new MessageFactory();
+            _commandfactory = new CommandFactory();
+            _debugcommandfactory = new DebugCommandFactory();
+            _apiService = new ApiService();
+
+            Levels = new Levels();
+            PlayerCache = new PlayerCache();
+            AllianceCache = new AllianceCache();
+            LeaderboardCache = new LeaderboardCache();
+
+            ChatManager = new LogicGlobalChatManager();
+
+            Gateway = new Gateway();
+        }
+
+        public static Gateway Gateway { get; set; }
+        public static PlayerCache PlayerCache { get; set; }
+        public static AllianceCache AllianceCache { get; set; }
+        public static LeaderboardCache LeaderboardCache { get; set; }
+        public static Configuration Configuration { get; set; }
+        public static Levels Levels { get; set; }
+        public static LogicGlobalChatManager ChatManager { get; set; }
+        public static Fingerprint Fingerprint { get; set; }
+        public static Csv Csv { get; set; }
+
+        public void Dispose()
+        {
+            Csv = null;
+            Gateway = null;
+            PlayerCache = null;
+            Configuration = null;
+            Levels = null;
+            Fingerprint = null;
+            _messagefactory = null;
+            _commandfactory = null;
+            _debugcommandfactory = null;
+            _mysql = null;
+            _apiService = null;
+            _logger = null;
+        }
+    }
+}
