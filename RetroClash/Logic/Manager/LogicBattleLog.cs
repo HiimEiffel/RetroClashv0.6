209af5625978fe59e68fd6a712e3da6f6d2ca44f
zq6A_qVXaE <84458031+HiimEiffel@users.noreply.github.com>
2021-12-12 20:21:08 +0800

    Add files via upload

---
+++ RetroClash/Logic/Manager/LogicBattleLog.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using RetroClash.Logic.Manager.Items;
+
+namespace RetroClash.Logic.Manager
+{
+    public class LogicBattleLog : List<BattleLog>
+    {
+    }
+}
