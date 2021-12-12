590df26306d272098df3c158ea861a827fd74750
zq6A_qVXaE <84458031+HiimEiffel@users.noreply.github.com>
2021-12-12 20:05:54 +0800

    lol infocuss

---
+++ RetroClash/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RetroClash")]
+[assembly: AssemblyDescription("A .NET Clash of Clans Server for the first Android version of coc (2013)")]
+[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("Incrediblee")]
+[assembly: AssemblyCompany("RetroClash")]
+[assembly: AssemblyProduct("RetroClash")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("f0069be1-7261-440b-8bc9-b3bac827ce1d")]
+[assembly: AssemblyVersion("0.6.0.1")]
+[assembly: AssemblyFileVersion("0.6.0.1")]
+[assembly: NeutralResourcesLanguage("en")]
