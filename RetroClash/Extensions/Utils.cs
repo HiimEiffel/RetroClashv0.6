43a237a4ee033d4a7d90baf540f7ca3942dbd8fd
zq6A_qVXaE <84458031+HiimEiffel@users.noreply.github.com>
2021-12-12 21:42:05 +0800

    Add files via upload

---
+++ RetroClash/Extensions/Utils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace RetroClash.Extensions
+{
+    public class Utils
+    {
+        public static string GenerateToken
+        {
+            get
+            {
+                var random = new Random();
+                var token = string.Empty;
+
+                for (var i = 0; i < 40; i++)
+                    token += "abcdefghijklmnopqrstuvwxyz0123456789"[random.Next(36)];
+
+                return token;
+            }
+        }
+
+        public static string GenerateRc4Key
+        {
+            get
+            {
+                var random = new Random();
+                var token = string.Empty;
+
+                for (var i = 0; i < 38; i++)
+                    token += "abcdefghijklmnopqrstuvwxyz0123456789"[random.Next(36)];
+
+                return token;
+            }
+        }
+
+        public static int GetSecondsUntilNextMonth
+        {
+            get
+            {
+                var now = DateTime.UtcNow;
+
+                if (now.Month != 12)
+                    return (int) (new DateTime(now.Year, now.Month + 1, 1, now.Hour,
+                                      now.Minute, now.Second) - now).TotalSeconds;
+
+                return (int) (new DateTime(now.Year, 1, 1, now.Hour,
+                                  now.Minute, now.Second) - now).TotalSeconds;
+            }
+        }
+    }
+}
